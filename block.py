bb204fc3b19f62ea6c22cc0757dfa90105b5cc6e
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-07-06 23:47:04 +0800

    wowowo

---
+++ block.py
@@ -0,0 +1,20 @@
+
+class Block(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
+    
