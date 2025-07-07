e8b43d041ec6875ba1058bfcc22cb92971b91d43
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-07-07 18:56:12 +0800

    update

--- block.py
+++ block.py
@@ -1,11 +1,22 @@
-
+import hashlib
+import json
+from time import time
 class Block(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        pass
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index' : len(self.chain) + 1,
+            'timestamp' : time(),
+            'transactions' : self.current_transactions,
+            'proof' : proof,
+            'previous_hash' : previous_hash or self.hash(self.chain[-1])
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         self.current_transactions.append({
@@ -19,9 +30,10 @@ class Block(object):
 
     @staticmethod
     def hash(block):
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
     
